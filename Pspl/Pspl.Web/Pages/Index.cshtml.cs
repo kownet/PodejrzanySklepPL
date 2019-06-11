6f75bbd2469540c50bf48313b7770e3699158e78
tkowalczyk <tomek@kownet.info>
2019-06-11 18:02:57 +0200

    web infrastructure

---
+++ Pspl/Pspl.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pspl.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
