4911612b255025b8c64efb647aa3a3e1d1ab515a
tkowalczyk <tomek@kownet.info>
2019-06-13 14:00:11 +0200

    web rename

---
+++ Pspl/Pspl.Web/wwwroot/js/home.js
@@ -0,0 +1,15 @@
+﻿APP.home = (function () {
+
+    var checkStore = function (opts) {
+
+        console.log(opts);
+
+    };
+
+    return {
+        checkStore: function (opts) {
+            checkStore(opts);
+        }
+    };
+
+})();
